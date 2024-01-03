c4a42f58faabfcca88877f667257a222946317e4
Pankaj Singh <121671526+pankajkscs@users.noreply.github.com>
2024-01-03 20:59:54 +0530

    fibnachi series

---
+++ FibNum.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class FibNum {
+    static long getFebNum(int n) {
+        long a = 0, b = 1, c;
+        if (n == 0)
+            return a;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getFebNum(10));
+    }
+}
