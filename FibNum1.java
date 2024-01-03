c5bfe122f29729655883b99ef091579253886b6c
Pankaj Singh <121671526+pankajkscs@users.noreply.github.com>
2024-01-03 20:58:52 +0530

    Fibonachiseries

---
+++ FibNum1.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import java.util.stream.Stream;
+
+public class FibNum1 {
+    private static void findFibonacciSeries() {
+
+        Stream.iterate(new int[] { 0, 1 }, f -> new int[] { f[1], f[0] + f[1] }).limit(10).map(f -> f[0])
+                .forEach(System.out::println);
+
+    }
+
+    public static void main(String[] args) {
+        findFibonacciSeries();
+    }
+}
