718cd624c60af76ae20484f8fb7cb06339590d1b
Pankaj Singh <121671526+pankajkscs@users.noreply.github.com>
2024-01-12 22:36:57 +0530

    majority element in an array.

---
+++ MajorityElementInStream.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class MajorityElementInStream {
+    public static void main(String[] args) {
+        int majorityArray[] = { 5, 3, 2, 1, 2, 4, 3, 2, 2, 6, 2, 3, 2, 2, 2 };
+        int notMajorityArray[] = { 3, 6, 32, 1, 8, 5, 31, 22 };
+        printMajorityElementByJava8(majorityArray);
+     //   printMajorityElementByJava8(notMajorityArray);
+    }
+
+    private static void printMajorityElementByJava8(int[] a) {
+        Integer unique = Arrays.stream(a).boxed()
+                .collect(Collectors.groupingBy(Function.identity(), HashMap::new, Collectors.counting())).entrySet()
+                .stream().filter(dt -> dt.getValue() >= a.length/2).map(Entry::getKey).findFirst().orElse(0);
+        System.out.println(unique);
+    }
+}
